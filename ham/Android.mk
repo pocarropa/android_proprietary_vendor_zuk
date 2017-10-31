2c1dc45161b21466055af1a24d5cf3971344be19
OrdenKrieger <OrdenKriegerEDR@googlemail.com>
2017-10-31 02:38:24 +0100

    ham: Move proprietary binaries to vendor
     *OEM proprietary blobs lay now in vendor since android 8
    
    Signed-off-by: OrdenKrieger <OrdenKriegerEDR@googlemail.com>

--- ham/Android.mk
+++ ham/Android.mk
@@ -22,11 +22,12 @@ ifeq ($(TARGET_DEVICE),ham)
 include $(CLEAR_VARS)
 LOCAL_MODULE := libwpa_qmi_eap_proxy
 LOCAL_MODULE_OWNER := zuk
-LOCAL_SRC_FILES := proprietary/lib/libwpa_qmi_eap_proxy.so
+LOCAL_SRC_FILES := proprietary/vendor/lib/libwpa_qmi_eap_proxy.so
 LOCAL_MULTILIB := 32
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 LOCAL_MODULE_SUFFIX := .so
+LOCAL_PROPRIETARY_MODULE := true
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
